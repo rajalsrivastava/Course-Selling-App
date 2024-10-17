91371d464b201791a16f82a23e23e348b8d098fa
Rajal Srivastava <rajal10srivastava@gmail.com>
2024-10-17 19:23:21 +0530

    added course and user routers

--- index.js
+++ index.js
@@ -1,33 +1,11 @@
 const express=require("express");
-const app=express;
+const { userRouter } = require("./routes/user");
+const { courseRouter } = require("./routes/course");
+const app=express();
 
-app.post("/user/signup", function(req,res){
-    res.json({
-        message: "signup endpoint"
-    })
-})
+app.use("/api/v1/user", userRouter);
+app.use("/api/v1/course", courseRouter);
 
-app.post("/user/signin", function(req,res){
-    res.json({
-        message: "signup endpoint"
-    })
-})
 
-app.get("/user/purchases", function(req,res){
-    res.json({
-        message: "signup endpoint"
-    })
-})
-app.post("/course/purchase", function(req,res){
-    // you would expect the user to pay you money
-    res.json({
-        message: "signup endpoint"
-    })
-})
-app.get("/courses", function(req,res){
-    res.json({
-        message: "signup endpoint"
-    })
-})
 
 app.listen(3000);
